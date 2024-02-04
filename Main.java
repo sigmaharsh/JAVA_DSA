521636cf765add23a4405eac6945822ea0888b3f
sigmaharsh <hgg030244@gmail.com>
2024-02-04 22:01:57 +0530

    changes in main.java

--- Main.java
+++ Main.java
@@ -1,10 +1,11 @@
 import java.util.Scanner;
 
 public class Main{
+
     public static void main(String[] args) {
 
 
-
+      //take input from user for array
         Scanner sc = new Scanner(System.in);
         int[][] arr = new int[3][3];
         for (int i = 0; i < arr.length; i++) {
@@ -15,6 +16,7 @@ public class Main{
 
 
         }
+        //this line of code prints the 2d array
         for (int[] ints : arr) {
             for (int j = 0; j < ints.length; j++) {
                 System.out.print(ints[j] + " ");
