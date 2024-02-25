7021054020688976c94c15fd5ace5f7d2c298ae4
sigmaharsh <hgg030244@gmail.com>
2024-02-25 12:11:27 +0530

    modified searching java file

--- Searching.java
+++ Searching.java
@@ -1,32 +1,38 @@
+import java.util.Scanner;
+
 public class Searching {
 
-    public static void main(String[] args) {
-        int[] arr = {11,33,44,43,34,3423,-1,55,66};
+  public static void main(String[] args) {
+    java.util.Scanner sc = new Scanner(System.in);
+    int n;
+    System.out.println("enter arrray size");
+    n = sc.nextInt();
+    int[] arr = new int[n];
+    System.out.print("enter array elements");
+    for (int i = 0; i < arr.length; i++) {
+      arr[i] = sc.nextInt();
+    }
+    System.out.println("enter target to search");
+    int target = sc.nextInt();
+    int ans = linearSearch(arr, target);
+    System.out.println("the target is present in the position : " + ans);
+  }
 
-        int target = 99;
-        int ans = linearSearch(arr,target);
-        System.out.println(ans);
+  // creating function which takes parameters an integer array and a target
+  static int linearSearch(int[] arr, int target) {
+    if (arr.length == 0) {
+      return -1;
     }
 
-// creating function
-        static  int linearSearch(int[] arr,int target){
-            if( arr.length == 0) {
-              return -1;
-            }
+    for (int i = 0; i < arr.length; i++) {
+      int element = arr[i];
+      if (element == target) {// we iterate the array and if the target is found return its positon in array
+        return i;
 
+      }
 
-            for (int i = 0; i < arr.length; i++) {
-                int element = arr[i];
-                if (element == target){
-                    return i ;
-
-            }
-
-
-        }
-         return -1;
     }
+    return -1;// return - 1 if the target is not found;
+  }
 
 }
-
-
