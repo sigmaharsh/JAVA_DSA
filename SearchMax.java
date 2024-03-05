dd5fea07ea0fe57ec55f34eb1deb47a364d36303
sigmaharsh <hgg030244@gmail.com>
2024-03-05 10:46:34 +0530

    updated a file

--- SearchMax.java
+++ SearchMax.java
@@ -1,16 +1,16 @@
-public class SearchMin {
+public class SearchMax {
 
     public static void main(String[] args) {
         //program can also be created by taking user input for the array
         int[] arr = {12,43,1,-3,5,6};
-        System.out.println(min(arr));
+        System.out.println(max(arr));
 
     }
-    static int min(int[] arr ){
+    static int max(int[] arr ){
         int ans = arr[0];
         for (int i = 1; i < arr.length ; i++)
         {
-           if(arr[i]< ans){
+           if(arr[i]> ans){
                ans = arr[i];
 
            }
